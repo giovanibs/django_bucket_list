14b5c33c5a6869cec230f4d1535dc01b41eebc0c
Giovani <giovanibulian@hotmail.com>
2023-06-17 18:51:57 -0400

    + signup and login

--- django_bucket_list/settings.py
+++ django_bucket_list/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'bucketapp',
+    'userapp',
     'crispy_forms',
     "crispy_bootstrap5",
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'userapp/signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'userapp/login.html', {'form': form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')

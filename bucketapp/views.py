2464b6eeac5719170e72a0aa35ae43c314ffc0b2
Giovani <giovanibulian@hotmail.com>
2023-06-19 20:39:23 -0400

    + permissions and alerts

--- bucketapp/views.py
+++ bucketapp/views.py
@@ -26,6 +26,20 @@ class TaskCreate(LoginRequiredMixin, CreateView):
         return context
     
     
+    def form_valid(self, form):
+        bucket = form.cleaned_data['bucket']
+        
+        if self.request.user != bucket.owner:
+            messages.error(self.request,
+                       "You are not the owner of this bucket.",
+                       # bootstrap tags
+                       extra_tags="alert alert-warning d-flex align-items-center"
+        )
+            return self.render_to_response(self.get_context_data(form=form))
+        
+        return super().form_valid(form)
+    
+    
 class BucketCreate(LoginRequiredMixin, CreateView):
     model = Bucket
     
@@ -63,25 +77,90 @@ class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.request.user != self.object.bucket.owner:
+            messages.error(self.request,
+                       "You are not the owner of this bucket.",
+                       # bootstrap tags
+                       extra_tags="alert alert-warning d-flex align-items-center"
+        )
+            referring_page = request.META.get('HTTP_REFERER')
+            return HttpResponseRedirect(referring_page or reverse('bucket-list'))
+
+        return super().dispatch(request, *args, **kwargs)
+    
     
 class BucketUpdate(LoginRequiredMixin, UpdateView):
     model = Bucket
     fields = '__all__'
     
     
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.request.user != self.object.owner:
+            messages.error(self.request,
+                       "You are not the owner of this bucket.",
+                       # bootstrap tags
+                       extra_tags="alert alert-warning d-flex align-items-center"
+        )
+            referring_page = request.META.get('HTTP_REFERER')
+            return HttpResponseRedirect(referring_page or reverse('bucket-list'))
+
+        return super().dispatch(request, *args, **kwargs)
+
+    
 class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     success_url = reverse_lazy('bucket-list')
+    
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.request.user != self.object.bucket.owner:
+            messages.error(self.request,
+                       "You are not the owner of this bucket.",
+                       # bootstrap tags
+                       extra_tags="alert alert-danger d-flex align-items-center"
+        )
+            referring_page = request.META.get('HTTP_REFERER')
+            return HttpResponseRedirect(referring_page or reverse('bucket-list'))
+
+        return super().dispatch(request, *args, **kwargs)
     
     
 class BucketDelete(LoginRequiredMixin, DeleteView):
     model = Bucket
     success_url = reverse_lazy('bucket-list')
     
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.request.user != self.object.owner:
+            messages.error(self.request,
+                       "You are not the owner of this bucket.",
+                       # bootstrap tags
+                       extra_tags="alert alert-danger d-flex align-items-center"
+        )
+            referring_page = request.META.get('HTTP_REFERER')
+            return HttpResponseRedirect(referring_page or reverse('bucket-list'))
+
+        return super().dispatch(request, *args, **kwargs)
+    
 
 @login_required 
 def task_toggle_complete(request, pk):
     task = get_object_or_404(Task, id=pk)
-    task.complete = not task.complete
-    task.save()
+    
+    if request.user in [task.assignee, task.bucket.owner]:
+        task.complete = not task.complete
+        task.save()
+    else:
+        messages.error(request,
+                       "You are not the owner of the bucket or the task's assignee.",
+                       # bootstrap tags
+                       extra_tags="alert alert-warning d-flex align-items-center"
+        )
     return HttpResponseRedirect(reverse('bucket-detail', args=[task.bucket.pk]))
